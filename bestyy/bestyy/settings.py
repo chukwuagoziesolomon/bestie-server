b42d09cac9688e5e47c56f7d7ca7d7769efac107
chukwuagoziesolomon <chukwuagoziesolomon.com>
2025-07-24 16:42:49 +0100

    changes

--- bestyy/bestyy/settings.py
+++ bestyy/bestyy/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from pathlib import Path
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,18 +94,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bestyy.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bestyy_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Frank08037',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(
+        config('DATABASE_URL', default='postgresql://postgres:Frank08037@localhost:5432/bestyy_db')
+    )
 }
 
 # Password validation
