dd44656f38f03668c760635c6cb588a243043a94
chukwuagoziesolomon <chukwuagoziesolomon@gmail.com>
2025-11-21 11:30:15 +0100

    Fix migration conflict: Recreate 0011_dedicatedvirtualaccount for Render deployment

--- bestyy/core_features/user/migrations/0012_systemsettings.py
+++ bestyy/core_features/user/migrations/0012_systemsettings.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0010_add_order_fields'),
+        ('user', '0011_dedicatedvirtualaccount'),
     ]
 
     operations = [
